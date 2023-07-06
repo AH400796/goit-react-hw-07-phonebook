2e023ada00b6795ddafb7484061804c12dabc81b
AH400796 <a.hokhman@gmail.com>
2023-07-06 18:46:47 +0300

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL =
-  'https://63f5d6d959c944921f67858f.mockapi.io/goit-react-hw-07-phonebook';
+axios.defaults.baseURL = 'https://63f5d6d959c944921f67858f.mockapi.io/';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
